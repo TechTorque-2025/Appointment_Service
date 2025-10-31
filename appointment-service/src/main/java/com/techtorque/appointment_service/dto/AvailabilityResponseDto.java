821e83c4e9a0dfd5d853368dc6e49bad5e77d283
ChamodiSandunika <chamodisandunika2002@gmail.com>
2025-10-31 15:21:04 +0530

    feat: Add AvailabilityResponseDto for representing service availability details

---
+++ appointment-service/src/main/java/com/techtorque/appointment_service/dto/AvailabilityResponseDto.java
@@ -0,0 +1,20 @@
+package com.techtorque.appointment_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AvailabilityResponseDto {
+
+  private LocalDate date;
+  private String serviceType;
+  private int durationMinutes;
+  private List<TimeSlotDto> availableSlots;
+}
